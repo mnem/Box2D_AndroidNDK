f596b918f63a3b22489780ea1f6bc1eb75c545b6
David Wagner <mnem@noiseandheat.com>
2010-05-24 00:36:02 +0100

    Initial pass at porting Box2D to Android NDK. No JNI interface is provided yet.
    Modified jni/Box2D/Source/Collision/b2DynamicTree.cpp:64 to add a cast to the
    correct data type so the template could be generated.

---
+++ jni/Android.mk
@@ -0,0 +1,58 @@
+BOX2D_SOURCES := Box2D/Source/Collision/b2BroadPhase.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2CollideCircle.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2CollideEdge.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2CollidePoly.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2Collision.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2Distance.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2DynamicTree.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2PairManager.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/b2TimeOfImpact.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/Shapes/b2CircleShape.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/Shapes/b2EdgeShape.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/Shapes/b2PolygonShape.cpp
+BOX2D_SOURCES += Box2D/Source/Collision/Shapes/b2Shape.cpp
+BOX2D_SOURCES += Box2D/Source/Common/b2BlockAllocator.cpp
+BOX2D_SOURCES += Box2D/Source/Common/b2Math.cpp
+BOX2D_SOURCES += Box2D/Source/Common/b2Settings.cpp
+BOX2D_SOURCES += Box2D/Source/Common/b2StackAllocator.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/b2Body.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/b2ContactManager.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/b2EdgeChain.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/b2Fixture.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/b2Island.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/b2World.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/b2WorldCallbacks.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Contacts/b2CircleContact.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Contacts/b2Contact.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Contacts/b2ContactSolver.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Contacts/b2EdgeAndCircleContact.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Contacts/b2PolyAndCircleContact.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Contacts/b2PolyAndEdgeContact.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Contacts/b2PolyContact.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Controllers/b2BuoyancyController.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Controllers/b2ConstantAccelController.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Controllers/b2ConstantForceController.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Controllers/b2Controller.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Controllers/b2GravityController.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Controllers/b2TensorDampingController.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2DistanceJoint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2FixedJoint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2GearJoint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2Joint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2LineJoint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2MouseJoint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2PrismaticJoint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2PulleyJoint.cpp
+BOX2D_SOURCES += Box2D/Source/Dynamics/Joints/b2RevoluteJoint.cpp
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE   := arm
+
+LOCAL_MODULE     := box2d
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/Box2DAndroid/includes
+LOCAL_SRC_FILES  := $(BOX2D_SOURCES)
+
+include $(BUILD_SHARED_LIBRARY)
